52b0aa1d9aade13f79582783001706f54eb800c4
KapitanMorkow <157925142+KapitanMorkow@users.noreply.github.com>
2026-09-24 03:04:08 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -979,6 +979,11 @@ let ru_2010_gr = [
 		pack : RU_2010_GR_PACK_1,
 		group : 'Хлеб',
 		song : "Курточка Stone Island (2017)"
+	},
+	{
+		pack : RU_2010_GR_PACK_2,
+		group : 'Обе-Рек',
+		song : "Золотая рыбка (2013)"
 	}
 ];
 
